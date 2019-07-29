d91fe29edcf72c6c4dbdd06d28537d0a0bfbe3cd
Jiehui Chen <599194429@qq.com>
2019-07-29 23:48:19 +0800

    add login verify and reminer

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Auth;
 
 class EmailController extends Controller
 {
@@ -11,13 +12,15 @@ class EmailController extends Controller
         $user = User::where('confirmation_token', $token)->first();
 
         if (is_null($user)) {
+            flash('验证失败！')->error();
             return redirect('/');
         }
         
         $user->is_active = 1;
         $user->confirmation_token = \str_random(40);
         $user->save();
-
+        Auth::login($user);
+        flash('验证成功！')->success();
         return redirect('/home');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,8 @@
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
+
+
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
@@ -70,11 +72,18 @@
                 </div>
             </div>
         </nav>
+        <div class="container">
+            @include('flash::message')
+        </div>
 
         @yield('content')
     </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+        $('#flash-overlay-modal').modal();
+    </script>
+
 </body>
 </html>
