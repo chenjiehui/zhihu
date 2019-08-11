b5f3db22900e504cf1ae1fb078bb0334d7c2fda2
Jiehui Chen <599194429@qq.com>
2019-08-11 23:31:20 +0800

    create topic model and relation betweent topics & questions

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -9,6 +9,11 @@ use App\Http\Requests\StoreQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
